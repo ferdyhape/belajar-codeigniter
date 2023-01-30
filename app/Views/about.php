5dfe22fe926159d3edbae191897c9bf26dcbee79
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-01-30 20:43:43 +0700

    add all view

---
+++ app/Views/about.php
@@ -0,0 +1,25 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('content') ?>
+
+<header class="p-5 text-primary rounded">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="h1">About us</h1>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi unde cumque! Cum repellendus eveniet, illum id doloribus, quibusdam tenetur debitis est libero quasi assumenda voluptates aliquam tempore. Porro, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi unde cumque! Cum repellendus eveniet, illum id doloribus, quibusdam tenetur debitis est libero quasi assumenda voluptates aliquam tempore. Porro, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi unde cumque! Cum repellendus eveniet, illum id doloribus, quibusdam tenetur debitis est libero quasi assumenda voluptates aliquam tempore. Porro, asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi unde cumque! Cum repellendus eveniet, illum id doloribus, quibusdam tenetur debitis est libero quasi assumenda voluptates aliquam tempore. Porro, asperiores.</p>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
